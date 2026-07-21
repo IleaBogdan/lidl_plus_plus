2b93bf2b90b4f11fb872e5787849ed06cd63f5ce
Ilea Bogdan <ilea.bogdan06@gmail.com>
2026-07-21 13:00:03 +0300

    frontend semi done

---
+++ frontend/src/api.js
@@ -0,0 +1,7 @@
+export const fetchMapData = async () => {
+  // Random function that returns nothing
+};
+
+export const submitProduct = async (productName) => {
+  // Random function that returns nothing
+};
